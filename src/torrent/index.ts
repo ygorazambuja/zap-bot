1facab04ab096faa910c96bae7532d449be9621c
Ygor Azambuja <ygorazambuja@gmail.com>
2021-10-08 13:39:05 -0400

    fix: muda validação do torrent

--- src/torrent/index.ts
+++ src/torrent/index.ts
@@ -1,17 +1,31 @@
 import { Client, Message } from "@open-wa/wa-automate";
 
-import torrentSearchApi from "torrent-search-api";
+import torrentSearchApi, { Torrent } from "torrent-search-api";
 
 torrentSearchApi.enableProvider("ThePirateBay");
 
 export async function searchTorrent(client: Client, message: Message) {
-  if (message?.body?.startsWith("!torrent")) {
+  if (message?.body?.startsWith("!torrent ")) {
     const query = message.body.split("!torrent ")[1];
     const torrents = await torrentSearchApi.search(query, "All", 1);
+    const [torrent] = torrents;
 
-    const createResponseMessage = (torrent) =>
-      `${torrent?.title}\n\n${torrent?.magnet}`;
+    if (torrent.numFiles === 0) {
+      return client.sendText(message.from, "Não foram encontrados resultados");
+    }
 
-    client.sendText(message.chatId, createResponseMessage(torrents[0]));
+    const createTorrentInfo = (torrent: Torrent) => {
+      return `
+        Título: ${torrent.title}
+        Tamanho: ${torrent.size}
+        Provider: ${torrent.provider}
+        Seeds: ${torrent.seeds}
+        Peers: ${torrent.peers}
+      `;
+    };
+
+    await client.sendText(message.chatId, createTorrentInfo(torrent));
+
+    return await client.sendText(message.chatId, torrent.magnet);
   }
 }
